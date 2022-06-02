9c70e0340c99cab07c07458861882c383887949f
Prajyot Koli <95091826+prajyotkoli1999@users.noreply.github.com>
2022-06-02 17:23:56 +0530

    Delete SpringProject directory

--- SpringProject/BookProject/src/main/java/com/example/BookProject/Repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BookProject.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.BookProject.Department.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-
-}
--- SpringProject/SpringDataMappingManyToMany/src/main/java/com/manytomanyrelationship/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manytomanyrelationship.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.manytomanyrelationship.entity.Users;
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Integer> { 
-}
